56f427b21ced5444048d3691232f29eb971a4e2e
marcelo <marc.cardo@gmail.com>
2016-06-25 19:03:42 -0300

    fix bug on landslide

--- js/world.js
+++ js/world.js
@@ -168,6 +168,7 @@ var World = Class.extend({
         for (var i = 0; i < 20; i++) {
             for (var j = 0; j < 20; j++) {
                 if(this.floodMatrix[i][j] == color){
+                    this.level[i][j] = 0;
                     this.floorCubes[i][j].position.setY(-2000);
                 }
             }
